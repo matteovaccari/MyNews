ef0ddebc3f052a2af42cf62df96d3dec3c68a281
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-05-24 12:47:24 +0200

    added comments and getPageTitle method who return each tab title

--- app/src/main/java/com/matt/android/mynews/adapters/PageAdapter.java
+++ app/src/main/java/com/matt/android/mynews/adapters/PageAdapter.java
@@ -1,23 +1,26 @@
 package com.matt.android.mynews.adapters;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.matt.android.mynews.R;
 import com.matt.android.mynews.controllers.fragments.ArtsFragment;
 import com.matt.android.mynews.controllers.fragments.MostPopularFragment;
 import com.matt.android.mynews.controllers.fragments.TopStoriesFragment;
 
 public class PageAdapter extends FragmentPagerAdapter {
 
-    
+    //Constructor
     public PageAdapter(FragmentManager mgr) {
         super(mgr);
     }
 
+    //Method who return a Fragment depending of ViewPager's position
     @Override
     public Fragment getItem(int position) {
-        switch(position){
+        switch (position) {
             case 0:
                 return TopStoriesFragment.newInstance();
             case 1:
@@ -29,8 +32,26 @@ public class PageAdapter extends FragmentPagerAdapter {
         }
     }
 
+    //Method who return the number of tabs
     @Override
     public int getCount() {
         return 3;
+    }
+
+    //Method who return each tab title name
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        CharSequence cs;
+        switch (position) {
+            case 0:
+                return cs = String.valueOf(R.string.top_stories_tab_name);
+            case 1:
+                return cs = String.valueOf(R.string.most_popular_tab_name);
+            case 2:
+                return cs = String.valueOf(R.string.arts_tab_name);
+            default:
+                return null;
+        }
     }
 }
