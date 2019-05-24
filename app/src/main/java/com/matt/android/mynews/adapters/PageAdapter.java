d29f7b29431a1c1b7eb5ebd9f2bb5e56bb277181
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-05-24 12:32:55 +0200

    added getItem method who return a fragment depending of adapter position

--- app/src/main/java/com/matt/android/mynews/adapters/PageAdapter.java
+++ app/src/main/java/com/matt/android/mynews/adapters/PageAdapter.java
@@ -4,15 +4,29 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.matt.android.mynews.controllers.fragments.ArtsFragment;
+import com.matt.android.mynews.controllers.fragments.MostPopularFragment;
+import com.matt.android.mynews.controllers.fragments.TopStoriesFragment;
+
 public class PageAdapter extends FragmentPagerAdapter {
 
+    
     public PageAdapter(FragmentManager mgr) {
         super(mgr);
     }
 
     @Override
     public Fragment getItem(int position) {
-        return ();
+        switch(position){
+            case 0:
+                return TopStoriesFragment.newInstance();
+            case 1:
+                return MostPopularFragment.newInstance();
+            case 2:
+                return ArtsFragment.newInstance();
+            default:
+                return null;
+        }
     }
 
     @Override
