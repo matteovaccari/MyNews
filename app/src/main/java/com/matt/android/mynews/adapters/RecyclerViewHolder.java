ab1172e7ecb28f9f4df1a79efd47955d6f950458
vaccari.matteo <matteo.vaccari@yahoo.com>
2019-06-12 14:58:02 +0200

    added every item that will be manipulated by recyclerView into the viewHolder + updated updateWithArticle method

--- app/src/main/java/com/matt/android/mynews/adapters/RecyclerViewHolder.java
+++ app/src/main/java/com/matt/android/mynews/adapters/RecyclerViewHolder.java
@@ -3,10 +3,12 @@ package com.matt.android.mynews.adapters;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.matt.android.mynews.R;
-import com.matt.android.mynews.models.Result;
+import com.matt.android.mynews.models.api.Result;
+import com.matt.android.mynews.models.utils.UpdateTextItems;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,14 +18,33 @@ import butterknife.ButterKnife;
  */
 public class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
-    @BindView(R.id.fragment_most_popular_item_title) TextView title;
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_item_title)
+    TextView textViewTitle;
+    @BindView(R.id.fragment_main_section)
+    TextView textViewSection;
+    @BindView(R.id.fragment_main_subsection)
+    TextView textViewSubSection;
+    @BindView(R.id.fragment_main_date)
+    TextView textViewDate;
+    @BindView(R.id.fragment_main_item_image)
+    ImageView imageView;
 
+    //Constructor
     public RecyclerViewHolder(@NonNull View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
     }
 
     public void updateWithArticle(final Result article) {
-        this.title.setText(article.getTitle());
+        UpdateTextItems update = new UpdateTextItems();
+        // set Section
+        this.textViewSection.setText(update.setSection(article));
+        // set Subsection
+        this.textViewSubSection.setText(update.setSubSection(article));
+        // set Title
+        this.textViewTitle.setText(update.setTitle(article));
+        // set Date
+        this.textViewDate.setText(update.setDate(article));
     }
 }
