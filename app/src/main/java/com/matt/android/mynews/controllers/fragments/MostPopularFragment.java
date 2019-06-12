eabb47e2495298722ff3aecf3c91c3673f086c79
vaccari.matteo <matteo.vaccari@yahoo.com>
2019-06-12 13:16:49 +0200

    added methods to manipulates articles content from MostPopular API and apply them to reyclerView

--- app/src/main/java/com/matt/android/mynews/controllers/fragments/MostPopularFragment.java
+++ app/src/main/java/com/matt/android/mynews/controllers/fragments/MostPopularFragment.java
@@ -3,21 +3,27 @@ package com.matt.android.mynews.controllers.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.matt.android.mynews.R;
+import com.matt.android.mynews.adapters.RecyclerViewAdapter;
 import com.matt.android.mynews.models.MainDataObservable;
 import com.matt.android.mynews.models.NYTStreams;
 import com.matt.android.mynews.models.Result;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableObserver;
 
@@ -28,6 +34,10 @@ public class MostPopularFragment extends Fragment {
 
     //For DATA
     private Disposable disposable;
+    private List<Result> resultList;
+    private RecyclerViewAdapter adapter;
+    //For Design
+    @BindView(R.id.most_popular_fragment_recyclerView) RecyclerView recyclerView;
 
     public static MostPopularFragment newInstance() {
         return (new MostPopularFragment());
@@ -38,7 +48,13 @@ public class MostPopularFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_most_popular, container, false);
+        View view = inflater.inflate(R.layout.fragment_most_popular, container, false);
+
+        ButterKnife.bind(this, view);
+
+        this.configureRecyclerView();
+        this.executeHttpRequest();
+        return view;
 
 
     }
@@ -65,7 +81,7 @@ public class MostPopularFragment extends Fragment {
 
             @Override
             public void onNext(MainDataObservable articles) {
-
+                updateUI(articles.getResults());
             }
 
             @Override
@@ -81,7 +97,7 @@ public class MostPopularFragment extends Fragment {
 
     }
 
-    private void disposeWhenDestroy(){
+    private void disposeWhenDestroy() {
         if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
     }
 
@@ -89,6 +105,7 @@ public class MostPopularFragment extends Fragment {
 
     /**
      * Method to sort and orgenize a list of dates
+     *
      * @param textArticle Dates of published articles
      */
     private void sortDateArray(final List<Result> textArticle) {
@@ -98,7 +115,7 @@ public class MostPopularFragment extends Fragment {
                     String date1 = textArticle1.getPublishedDate();
                     String date2 = textArticle2.getPublishedDate();
                     int result = date1.compareTo(date2);
-                    if(result == 0){
+                    if (result == 0) {
                         return date1.compareTo(date2);
                     }
                     return result;
@@ -110,13 +127,22 @@ public class MostPopularFragment extends Fragment {
     }
 
     protected void updateUI(List<Result> textArticle) {
-        mResultList.clear();
+        resultList.clear();
         sortDateArray(textArticle);
-        mResultList.addAll(textArticle);
+        resultList.addAll(textArticle);
         // reverse the list to have an ascendant list of date in RecyclerView
-        Collections.reverse(mResultList);
-        Log.i("List reverse", "List finale" + mResultList);
+        Collections.reverse(resultList);
+        Log.i("List reverse", "List finale" + resultList);
         adapter.notifyDataSetChanged();
+    }
+
+    private void configureRecyclerView() {
+        this.resultList = new ArrayList<>();
+        adapter = new RecyclerViewAdapter(resultList);
+        this.recyclerView.setAdapter(this.adapter);
+
+        // Set layout manager to position the items
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
 }
