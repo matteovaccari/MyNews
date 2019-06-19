26dccd7ae4c74b041473e681f8f1d62bf957f9af
vaccari.matteo <matteo.vaccari@yahoo.com>
2019-06-19 11:07:08 +0200

    added onCreateOptionMenu to inflate back button

--- app/src/main/java/com/matt/android/mynews/controllers/activities/SearchActivity.java
+++ app/src/main/java/com/matt/android/mynews/controllers/activities/SearchActivity.java
@@ -3,6 +3,7 @@ package com.matt.android.mynews.controllers.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.matt.android.mynews.R;
 
@@ -12,8 +13,19 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        
+
         this.configureToolBar();
+    }
+
+    /**
+     * Inflate the menu and add it to the toolbar
+     * @param menu BackButton
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_search, menu);
+        return true;
     }
 
     /**
