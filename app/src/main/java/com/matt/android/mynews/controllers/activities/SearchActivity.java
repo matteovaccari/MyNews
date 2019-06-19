44aadce0d1de712085ddca423e525b76d024106e
vaccari.matteo <matteo.vaccari@yahoo.com>
2019-06-19 10:58:45 +0200

    added configureToolbar method

--- app/src/main/java/com/matt/android/mynews/controllers/activities/SearchActivity.java
+++ app/src/main/java/com/matt/android/mynews/controllers/activities/SearchActivity.java
@@ -2,6 +2,7 @@ package com.matt.android.mynews.controllers.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.matt.android.mynews.R;
 
@@ -11,5 +12,15 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        
+        this.configureToolBar();
+    }
+
+    /**
+     * Get and set toolbar
+     */
+    private void configureToolBar(){
+        Toolbar toolbar = findViewById(R.id.search_activity_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
