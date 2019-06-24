89d21b8680b7a27e75daed37335b644a77168f99
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-06-24 17:08:24 +0200

    added temporary ActivityResult

---
+++ app/src/main/java/com/matt/android/mynews/controllers/activities/ResultActivity.java
@@ -0,0 +1,15 @@
+package com.matt.android.mynews.controllers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.matt.android.mynews.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
