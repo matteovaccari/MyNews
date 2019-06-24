ccb8a828942b7ffbf920afb35e16f7a52baa58d4
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-06-24 16:29:05 +0200

    added OnButtonClickedListener, getEditTextForQuery, getQuery and getCheckBox methods

--- app/src/main/java/com/matt/android/mynews/controllers/fragments/search/SearchFragment.java
+++ app/src/main/java/com/matt/android/mynews/controllers/fragments/search/SearchFragment.java
@@ -2,8 +2,12 @@ package com.matt.android.mynews.controllers.fragments.search;
 
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,15 +16,19 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.matt.android.mynews.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.app.SearchManager.QUERY;
 
 /**
  * Fragment who will contain all search-related options (date,key words, section)
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener {
 
     // --- FOR DESIGN
     //For date (begin + end date)
@@ -61,8 +69,8 @@ public class SearchFragment extends Fragment {
     private String dateBegin;
     private String endDate;
     // Query & Filter
-    private String mQuery;
-    private String mFilter;
+    private String query;
+    private String filter;
     private SearchFragment.OnButtonClickedListener callback;
     private int screenId;
 
@@ -77,7 +85,96 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+        //Initialize ButterKnife
+        ButterKnife.bind(this, view);
+
+        this.getEditTextForQuery();
+
         return view;
     }
 
+    /**
+     * Override method from View.OnClickListener, things to do when user click on Search Button
+     * @param view View
+     */
+    @Override
+    public void onClick(View view) {
+        // CheckBox
+        getCheckBox();
+        callback.onButtonClicked(view);
+        Intent intentResultActivity = new Intent(getActivity(), ResultActivity.class); //Result Activity have to be created
+        // put data into activity
+        intentResultActivity.putExtra(QUERY, getQuery());
+        intentResultActivity.putExtra(DATE_BEGIN, dateBeginForData);
+        intentResultActivity.putExtra(END_DATE, endDateForData);
+        intentResultActivity.putExtra(FILTER, mFilter);
+
+        if (!mFilter.equals("")) {
+            startActivity(intentResultActivity);
+        }
+    }
+    /**
+     * Configure CheckBox
+     */
+    protected void getCheckBox() {
+        filter = "";
+        if (artsCheckBox.isChecked()) {
+            filter = "Arts";
+        }
+        if (bussinessCheckBox.isChecked()) {
+            filter = "Business";
+        }
+        if (entrepreneursCheckBox.isChecked()) {
+            filter = "Entrepreneurs";
+        }
+        if (politicsCheckBox.isChecked()) {
+            filter = "Politics";
+        }
+        if (sportsCheckBox.isChecked()) {
+            filter = "Sports";
+        }
+        if (travelCheckBox.isChecked()) {
+            filter = "Travel";
+        }
+        if (filter.equals("")) {
+            Toast.makeText(getContext(), R.string.error_message_search_fragment, Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * Button Listener
+     */
+    public interface OnButtonClickedListener {
+        void onButtonClicked(View view);
+    }
+
+    /**
+     * Enable Search Button when user typed something in edit text
+     */
+    private void getEditTextForQuery() {
+        //Edit text
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text, int start, int before, int count) {
+                searchButton.setEnabled(text.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+    /**
+     * Method who return what user typed
+     * @return String query
+     */
+    private String getQuery() {
+        query = "";
+        query = editText.getText().toString();
+        Log.i("Query", query);
+        return query;
+    }
 }
