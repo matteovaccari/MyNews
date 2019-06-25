6ace95266974c74c4049a2d437fd693034746e18
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-06-25 11:10:45 +0200

    added Logger class for better debogging

---
+++ app/src/main/java/com/matt/android/mynews/models/utils/Logger.java
@@ -0,0 +1,10 @@
+package com.matt.android.mynews.models.utils;
+
+import android.util.Log;
+
+public class Logger {
+
+    public static void e(String message) {
+        Log.e("TAG", message);
+    }
+}
