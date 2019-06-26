ede0baa24873e619dbf5fad1e18653d4a664e7d1
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-06-26 12:42:04 +0200

    added checkConditions ( to verify that user as at least selected 1 categorie and 1 word + save and getUrl methods

--- app/src/main/java/com/matt/android/mynews/models/utils/SharedPreferencesManager.java
+++ app/src/main/java/com/matt/android/mynews/models/utils/SharedPreferencesManager.java
@@ -2,6 +2,7 @@ package com.matt.android.mynews.models.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import static com.matt.android.mynews.models.utils.Constants.PREF_KEY_NAME;
 
@@ -9,6 +10,12 @@ import static com.matt.android.mynews.models.utils.Constants.PREF_KEY_NAME;
  * Util class to manage SharedPreferences
  */
 public class SharedPreferencesManager {
+
+    private String url = "";
+    private String query;
+    private String beginDate = "";
+    private String endDate = "";
+    private String checkboxQuery = "";
 
     private SharedPreferences preferences;
     private Context context;
@@ -18,10 +25,42 @@ public class SharedPreferencesManager {
         preferences = context.getSharedPreferences(PREF_KEY_NAME, Context.MODE_PRIVATE);
         this.context = context;
     }
+    
 
-    public void saveUrl(){
-
+    /**
+     * Save the url in shared preferences
+     *
+     * @param key the key you want to associate the url
+     */
+    public void saveUrl(String key){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(key, url);
+        editor.apply();
+        checkboxQuery = "";
+        Logger.e("url = " + url);
     }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public void clearInput() {
+        checkboxQuery = "";
+        url = "";
+        query = "";
+        beginDate = "";
+        endDate = "";
+    }
+
+    /**
+     * Check that the user has chosen at least one checkbox category and one search query
+     *
+     * @return true if conditions are met
+     */
+    public boolean checkConditions() {
+        return (!checkboxQuery.isEmpty() && !query.isEmpty());
+    }
+
 
 
 }
