7db9de2ea44e562e6b680f85e02c8cfdac0c8d26
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-07-01 11:35:02 +0200

    added Toolbar for resultActivity

--- app/src/main/java/com/matt/android/mynews/controllers/activities/ResultActivity.java
+++ app/src/main/java/com/matt/android/mynews/controllers/activities/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.matt.android.mynews.controllers.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.matt.android.mynews.R;
 
@@ -12,5 +14,23 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        this.configureToolBar();
+    }
+
+    /**
+     * Get and set toolbar
+     */
+    private void configureToolBar(){
+        Toolbar toolbar = findViewById(R.id.toolbar_main_activity);
+        setSupportActionBar(toolbar);
+
+        //This is for back button
+        //Get a support Action bar corresponding to above toolbar
+        ActionBar actionBar = getSupportActionBar();
+        //Enable the up button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
