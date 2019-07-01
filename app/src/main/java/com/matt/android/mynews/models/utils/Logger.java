937a2b891ae68dae08f5b2fbb173c9268fb8c444
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-07-01 14:21:56 +0200

    added methods for warn and info logger util

--- app/src/main/java/com/matt/android/mynews/models/utils/Logger.java
+++ app/src/main/java/com/matt/android/mynews/models/utils/Logger.java
@@ -2,9 +2,22 @@ package com.matt.android.mynews.models.utils;
 
 import android.util.Log;
 
+/**
+ * Class who manage logs
+ */
 public class Logger {
 
+   //Blocking problem
     public static void e(String message) {
         Log.e("TAG", message);
+    }
+
+    //Non-Blocking problem
+    public static void w(String message) {
+        Log.w("TAG", message);
+    }
+    
+    public static void i(String message) {
+        Log.i("TAG", message);
     }
 }
