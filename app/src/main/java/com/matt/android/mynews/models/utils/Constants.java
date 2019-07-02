107f15829d78ea92cf4b73562875a902a82d4be0
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-07-02 14:38:08 +0200

    added constants for notifications

--- app/src/main/java/com/matt/android/mynews/models/utils/Constants.java
+++ app/src/main/java/com/matt/android/mynews/models/utils/Constants.java
@@ -5,6 +5,20 @@ package com.matt.android.mynews.models.utils;
  */
 public class Constants {
 
+    //Search util
     public static final String PREF_KEY_NAME = "PREF_KEY_NAME";
     public static final String PREF_KEY_SEARCH = "PREF_KEY_SEARCH";
+
+    //Notification util
+    public static final String PREF_KEY_SWITCH = "PREF_KEY_SWITCH";
+    public static final String PREF_KEY_QUERY = "PREF_KEY_QUERY";
+    public static final String PREF_KEY_ARTS = "PREF_KEY_ARTS";
+    public static final String PREF_KEY_POLITICS = "PREF_KEY_POLITICS";
+    public static final String PREF_KEY_ENTREPRENEURS = "PREF_KEY_ENTREPRENEURS";
+    public static final String PREF_KEY_BUSINESS = "PREF_KEY_BUSINESS";
+    public static final String PREF_KEY_SPORTS = "PREF_KEY_SPORTS";
+    public static final String PREF_KEY_TRAVEL = "PREF_KEY_TRAVEL";
+    public static final String PREF_KEY_NOTIFICATION_URL = "PREF_KEY_NOTIFICATION_URL";
+
+
 }
