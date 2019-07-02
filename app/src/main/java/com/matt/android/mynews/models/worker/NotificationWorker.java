2799be8aff7fdec487015db5dea1b9b416ff912c
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-07-02 16:27:57 +0200

    implemented core methods

--- app/src/main/java/com/matt/android/mynews/models/worker/NotificationWorker.java
+++ app/src/main/java/com/matt/android/mynews/models/worker/NotificationWorker.java
@@ -1,6 +1,26 @@
 package com.matt.android.mynews.models.worker;
 
-public class NotificationWorker {
+import android.content.Context;
+import android.support.annotation.NonNull;
 
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+
+public class NotificationWorker extends Worker {
+
+    public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        return Result.success();
+    }
 }
