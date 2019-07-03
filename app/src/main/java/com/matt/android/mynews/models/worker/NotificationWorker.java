c5cb3e5dd6402089c8315af3d1a16862cdd26cea
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-07-03 13:58:53 +0200

    added content for NotificationWorker : sendNotification, scheduleNotif and cancelReminder methods

--- app/src/main/java/com/matt/android/mynews/models/worker/NotificationWorker.java
+++ app/src/main/java/com/matt/android/mynews/models/worker/NotificationWorker.java
@@ -1,27 +1,48 @@
 package com.matt.android.mynews.models.worker;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
+import androidx.work.Data;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.matt.android.mynews.R;
+import com.matt.android.mynews.controllers.activities.MainActivity;
 import com.matt.android.mynews.models.api.NYTStreams;
 import com.matt.android.mynews.models.api.search.NewsObject;
+import com.matt.android.mynews.models.utils.Constants;
 import com.matt.android.mynews.models.utils.Logger;
 import com.matt.android.mynews.models.utils.SharedPreferencesManager;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableObserver;
 
+import static com.matt.android.mynews.models.utils.Constants.ID_WORKER_NOTIFICATION;
 import static com.matt.android.mynews.models.utils.Constants.PREF_KEY_NAME;
 import static com.matt.android.mynews.models.utils.Constants.PREF_KEY_NOTIFICATION_URL;
+import static com.matt.android.mynews.models.utils.Constants.TAG_WORKER;
 
 
 public class NotificationWorker extends Worker {
 
     private int hits;
     private Disposable disposable;
+    private String message;
 
     public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -30,7 +51,11 @@ public class NotificationWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-            doNotificationApiRequest();
+
+        int id = (int) getInputData().getLong(ID_WORKER_NOTIFICATION, 0);
+
+        sendNotification("test notif", id);
+
         return Result.success();
     }
 
@@ -46,14 +71,14 @@ public class NotificationWorker extends Worker {
             @Override
             public void onNext(NewsObject news) {
                 hits = news.checkIfResult();
-                createNotification(hits);
+               // sendNotification(hits);
             }
 
             @Override
             public void onError(Throwable e) {
                 //Create notification with error message
                 hits = -1;
-                createNotification(hits);
+               // sendNotification(hits);
                 Logger.e(e.getMessage());
             }
 
@@ -64,6 +89,43 @@ public class NotificationWorker extends Worker {
         });
 
     }
-    
+
+    public static void scheduleReminder(String tag) {
+        PeriodicWorkRequest.Builder notificationWork = new PeriodicWorkRequest.Builder(NotificationWorker.class, 1, TimeUnit.DAYS);
+        PeriodicWorkRequest request = notificationWork.build();
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(tag, ExistingPeriodicWorkPolicy.KEEP , request);
+    }
+
+
+    public static void cancelReminder(String tag) {
+        WorkManager instance = WorkManager.getInstance();
+        instance.cancelAllWorkByTag(tag);
+    }
+
+    private void sendNotification(String text, int id) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra(TAG_WORKER, id);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+        NotificationManager notificationManager = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("default", "Default", NotificationManager.IMPORTANCE_DEFAULT);
+            Objects.requireNonNull(notificationManager).createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), "default")
+                //Constant title from string resources
+                .setContentTitle(getApplicationContext().getResources().getString(R.string.notification_title))
+                .setContentText(text)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        Objects.requireNonNull(notificationManager).notify(id, notification.build());
+    }
 
 }
